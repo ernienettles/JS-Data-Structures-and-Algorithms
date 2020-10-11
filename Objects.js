7093f103bcc7f6c8b49871d940aa11d7041c59c4
Ernie Nettles <ernienettles@gmail.com>
2020-10-10 22:15:08 -0400

    Object notes done

---
+++ Objects.js
@@ -0,0 +1,15 @@
+let instructor = {
+    firstName: "Kelly",
+    isInstructor: true,
+    favoriteNumbers: [1, 2, 3, 4]
+}
+
+// Methods
+
+Object.keys(instructor) // Outputs an array with the keys within the instructor object
+
+Object.values(instructor) // Outputs an array with the values within the instructor object
+
+Object.entries(instructor) // Outputs an array of arrays that contain each key/value pair
+
+instructor.hasOwnProperty("firstName") // Outputs true or false depending on whether the key exists within the object
